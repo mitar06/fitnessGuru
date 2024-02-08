7c15efe76f1094fd60a6ae53190da7d24030298d
Nikola <nikola@giveologie.com>
2024-02-08 16:01:00 +0100

    initial Wagtail Commit

---
+++ fitnesssite/fitnesssite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-mw&gr*orj_(892bpjrxd@cz(p)nz(9glull(2r&^@e3u4@c@et"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
