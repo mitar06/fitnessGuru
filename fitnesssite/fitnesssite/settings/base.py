3df3a924c91a54fce79d6d0448d3bac3fec19fc2
Nikola <nikola@giveologie.com>
2024-02-14 14:08:12 +0100

    Added automatic wagtail menus, updated requirements.txt

--- fitnesssite/fitnesssite/settings/base.py
+++ fitnesssite/fitnesssite/settings/base.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "wagtail_modeladmin",
+    "wagtailmenus",
     # Theme Specific
     "tailwind",
     "theme",
