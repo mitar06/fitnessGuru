6fd5801352fb6be3f211f3ad03acddcc3c8843eb
Nikola <nikola@giveologie.com>
2024-02-22 18:58:41 +0100

    Edited Production Settings to take .env into account

--- fitnesssite/fitnesssite/settings/production.py
+++ fitnesssite/fitnesssite/settings/production.py
@@ -1,6 +1,13 @@
 from .base import *
 
-DEBUG = False
+DEBUG = bool(os.environ.get("DEBUG", False))
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 try:
     from .local import *
