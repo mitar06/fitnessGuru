28b90ef57181ae27f9b5fb253bad08d926e8da46
Nikola <nikola@giveologie.com>
2024-02-22 18:12:39 +0100

    added getting DEBUG variable for the .env file

--- fitnesssite/fitnesssite/settings/base.py
+++ fitnesssite/fitnesssite/settings/base.py
@@ -195,7 +195,7 @@ WAGTAILSEARCH_BACKENDS = {
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-WAGTAILADMIN_BASE_URL = "http://example.com"
+WAGTAILADMIN_BASE_URL = "http://fitnessguru.rs"
 
 
 INTERNAL_IPS = ["127.0.0.1"]
--- fitnesssite/fitnesssite/settings/dev.py
+++ fitnesssite/fitnesssite/settings/dev.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", True))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
