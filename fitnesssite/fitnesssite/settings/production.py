fd7b43bffba5bd68a49e484a9e03c411f7f7e93d
Nikola <nikola@giveologie.com>
2024-02-23 17:45:09 +0100

    added secure Cookie settings

--- fitnesssite/fitnesssite/settings/production.py
+++ fitnesssite/fitnesssite/settings/production.py
@@ -9,6 +9,9 @@ CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
 try:
     from .local import *
 except ImportError:
